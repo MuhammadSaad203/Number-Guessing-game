fa3fc3dfd30da2ebb06eea910a22a9937cfb1a57
Muhammad Saad <160244669+MuhammadSaad203@users.noreply.github.com>
2024-03-26 17:03:52 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const prizeNUmber: number = Math.floor(Math.random() * 6 + 1);
+const guessing = await inquirer.prompt([
+  { name: "userNumber", type: "number", message: "Please enter your Number between 1-6:" },
+]);
+if (guessing.userNumber === prizeNUmber) {
+  console.log("You have entered the right number");
+} else {
+  console.log("You have entered the wrong number");
+}
